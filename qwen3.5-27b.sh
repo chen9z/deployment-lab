7c2b222d2295e3835a5bec01b9145bf5dd724536
chen9z <yclooper@gmail.com>
2026-03-03 15:28:05 +0800

    update script

--- qwen3.5-27b.sh
+++ qwen3.5-27b.sh
@@ -4,7 +4,7 @@ source .venv/bin/activate
 
 vllm serve Kbenkhaled/Qwen3.5-27B-NVFP4 --served-model-name=qwen3.5-27b \
 --tensor-parallel-size=1 \
---max-model-len=32768 \
+--max-model-len=131072 \
 --max-num-seqs=1 \
 --enable-auto-tool-choice \
 --kv-cache-dtype fp8 \
@@ -12,9 +12,9 @@ vllm serve Kbenkhaled/Qwen3.5-27B-NVFP4 --served-model-name=qwen3.5-27b \
 --reasoning-parser qwen3 \
 --enable-prefix-caching \
 --attention-backend FLASHINFER \
---gpu-memory-utilization=0.95 \
+--gpu-memory-utilization=0.85 \
 --limit-mm-per-prompt.video 0 \
 --host=0.0.0.0 --port=8001 \
---speculative-config '{"method":"qwen3_next_mtp","num_speculative_tokens":2}' \
+--speculative-config '{"method":"qwen3_next_mtp","num_speculative_tokens":1}' \
 --generation-config auto \
 --override-generation-config '{"temperature":0.6,"top_p":0.95,"top_k":20,"min_p":0.0,"presence_penalty":0.0,"repetition_penalty":1.0}'
